4165964b2aaf9db2d741caf447a5b33cd70db248
Allen Christian Tubo <allen@quickreach.co>
2024-08-24 20:17:59 +0800

    Updated portfolio

--- my-portfolio/src/app/app.component.ts
+++ my-portfolio/src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { SplashScreenComponent } from './core/components/splash-screen/splash-screen.component';
+import { SplashScreenService } from './core/components/splash-screen/splash-screen.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SplashScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+  private unsubscribe: Subscription[] = [];
+  router = inject(Router);
+  splashScreenService = inject(SplashScreenService);
   title = 'my-portfolio';
+
+  ngOnInit(): void {
+    const routerSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // clear filtration paginations and others
+        // hide splash screen
+        this.splashScreenService.hide();
+        
+
+        // scroll to top on every route change
+        window.scrollTo(0, 0);
+
+        // to display back the body content
+        setTimeout(() => {
+          document.body.classList.add('page-loaded');
+        }, 500);
+      }
+    });
+    this.unsubscribe.push(routerSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.forEach((sb) => sb.unsubscribe());
+  }
 }
--- my-portfolio/src/app/app.routes.ts
+++ my-portfolio/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/pages/login/login.component';
+import { NotFoundScreenComponent } from './core/components/not-found-screen/not-found-screen.component';
+import { LayoutComponent } from './core/components/layout/layout.component';
+import { AboutComponent } from './core/pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LayoutComponent,
+        // children: [
+        //     {
+        //         path: "",
+        //         component: 
+        //     },
+        // ]
+    },
+    {
+        path: "**",
+        component: NotFoundScreenComponent
+    }
+];
